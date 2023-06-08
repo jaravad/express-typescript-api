47c03e8294a515975c71e5bb3d287cac31d8d60e
Jesus Santiago <jesusdavid2607@gmail.com>
2023-06-08 16:55:30 -0500

    Get specific diary entry, add new entry type

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,9 +1,15 @@
 import express from 'express'
+import * as diaryServices from '../services/diaryServices'
 
 const router = express.Router()
 
 router.get('/', (_req, res) => {
-  res.send('Fetching all diary entries')
+  res.send(diaryServices.getEntriesWithoutSensitiveData())
+})
+
+router.get('/:id', (req, res) => {
+  const entry = diaryServices.findById(+req.params.id)
+  return (entry != null) ? res.send(entry) : res.sendStatus(404)
 })
 
 router.post('/', (_req, res) => {
--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -5,6 +5,19 @@ const entries: DiaryEntry[] = diaryEntries as DiaryEntry[]
 
 export const getEntries = (): DiaryEntry[] => entries
 
-export const getEntryWithoutSensitiveData = (): NonSensitiveInfoDiaryEntry[] => entries
+export const getEntriesWithoutSensitiveData = (): NonSensitiveInfoDiaryEntry[] => {
+  return entries.map(({ id, date, weather, visibility }) => {
+    return { id, date, weather, visibility }
+  })
+}
+
+export const findById = (id: number): NonSensitiveInfoDiaryEntry | undefined => {
+  const entry = entries.find(d => d.id === id)
+  if (entry != null) {
+    const { comment, ...restOfEntry } = entry
+    return restOfEntry
+  }
+  return undefined
+}
 
 export const addEntry = (): null => null
